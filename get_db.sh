99fd041fc5263aa32548699e70a3dc7ffc911daa
Dinesh <dinesh@dinesh-exceleron.(none)>
2013-12-04 17:23:35 +0530

    db update

--- get_db.sh
+++ get_db.sh
@@ -1,6 +1,7 @@
-#! /usr/bin
-
-mongodump -d test -c expenses db/test;
-git add db/test/*;
+sudo mongodump -d test -c expenses db;
+echo "Backed up data..";
+git add *;
 git commit -m "db update";
+echo "Pushing to git repo..";
 git push origin master;
+echo "Pushed.$?";
