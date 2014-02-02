d76c750ae6757e3cf1809e14363dfdf0e16b1697
Dinesh <dd.dinesh.rajakumar@gmail.com>
2014-02-02 14:30:42 +0530

    * Added notification for success add. * Added more custom date filter option for status. * Added details column. * Delete will refresh the page.

--- routes/expenses.js
+++ routes/expenses.js
@@ -10,159 +10,201 @@ var ObjectID = require('mongodb').ObjectID;
 //var BSON = require('mongodb').BSONPure;
 var assert = require('assert');
 
-var db = new Db('test', new Server("127.0.0.1", 27017), { w:0, native_parser : false });
+var db = new Db('test', new Server("127.0.0.1", 27017), {
+    w: 0,
+    native_parser: false
+});
 
 
-exports.expense_list = function(req,res){
-db.open(function(err,db){
-                db.collection('expenses',function(err, collection){
-                        collection.find().toArray(function(err, data){
-                                db.close();
-                                res.render('expenses',{data : data, title : 'Expenses'});
-                                });
-                        });
+exports.expense_list = function(req, res) {
+    db.open(function(err, db) {
+        db.collection('expenses', function(err, collection) {
+            collection.find().toArray(function(err, data) {
+                db.close();
+                res.render('expenses', {
+                    data: data,
+                    title: 'Expenses'
                 });
+            });
+        });
+    });
 }
 
-exports.update = function(req,res){
-	res.render('expense_entry',{data :  {name : '', forWhat : '', howMuch : '' }});
+exports.update = function(req, res) {
+    res.render('expense_entry', {
+        data: {
+            name: '',
+            forWhat: '',
+            howMuch: '',
+            added_success: false
+        }
+    });
 }
 
 // under progress
-exports.edit = function(req,res){
-	var data = find_rec(req.body._id);
-	res.render('expense_entry',{ 'data' : data });
-	console.log(data);
+exports.edit = function(req, res) {
+    var data = find_rec(req.body._id);
+    res.render('expense_entry', {
+        'data': data
+    });
+    console.log(data);
 }
 
-exports.delete_rec = function(req, res){
-	db.open(function(err,db){
-        if(!err){
-                db.collection('expenses',function(err, col){
-                if(!err){
-                        col.remove({ "_id" : new ObjectID(req.body._id) },function(err){
+exports.delete_rec = function(req, res) {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection('expenses', function(err, col) {
+                if (!err) {
+                    col.remove({
+                        "_id": new ObjectID(req.body._id)
+                    }, function(err) {
                         db.close();
-                        if(!err){
-                                console.log("Deleted record "+ req.body._id + " Successfuly");
-                                exports.expense_list(req,res);
+                        if (!err) {
+                            console.log("Deleted record " + req.body._id + " Successfuly");
+                            exports.expense_list(req, res);
                         }
-                        });
+                    });
                 }
-                });
+            });
         }
-        });
+    });
 }
 
-exports.insert_expense = function(req,res){
-	db.open(function(err,db){
-        if(!err){
-          db.collection('expenses',function(err, collection){
-          if(!err){
-            //var date = req.body.date.split('/');
-            collection.insert({date : toDate(req.body.date),
-                               name : req.body.name,
-                               forWhat : req.body.forWhat,
-                               howMuch : req.body.howMuch },function(err){
-                                        db.close();
-                                        if(!err){
-						exports.expense_list(req,res);
-                                        }
-                                });
-          }//end of collection if
-          });
-        }//end of open if
-        else{
-                console.log("Error: "+err)
+exports.insert_expense = function(req, res) {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection('expenses', function(err, collection) {
+                if (!err) {
+                    //var date = req.body.date.split('/');
+                    collection.insert({
+                        date: toDate(req.body.date),
+                        name: req.body.name,
+                        forWhat: req.body.forWhat,
+						details: req.body.details,
+                        howMuch: req.body.howMuch
+                    }, function(err) {
+                        db.close();
+                        if (!err) {
+                            res.render('expense_entry', {
+                                data: {
+                                    name: req.body.name,
+                                    forWhat: req.body.forWhat,
+                                    howMuch: '',
+                                    added_success: true
+                                }
+                            });
+                        }
+                    });
+                } //end of collection if
+            });
+        } //end of open if
+        else {
+            console.log("Error: " + err)
         }
-        });
+    });
 }
 
 exports.stats = function(req, res) {
-db.open(function(err,db){
-                db.collection('expenses',function(err, collection){
-			var search_query = {};
-			if(req.query.full != 'yes'){
-			var y = req.query.y ? req.query.y : new Date().getFullYear();
-			var m = req.query.m ? req.query.m : (new Date().getMonth() + 1);
-			var d = req.query.d ? req.query.d : '01';
-			req.query.y = y;
-			req.query.m = m;
-			var start = toDate(d+'/'+m+'/'+y);
-			if(m == 12 ){
-				y = parseInt(y) + 1;
-				m = 0;
-			}
-			var end = toDate('01'+'/'+(Number(m)+1)+'/'+y);
-			search_query.date = {$gte: start, $lt: end};
-			}
-                        collection.find(search_query).toArray(function(err, data){
-                                db.close();
-				var user_vise = {};
-				var item_vise = {};
-				for(var i=0;i<data.length;i++){
-				    var name = data[i].name;
-				    var inr = Number(data[i].howMuch) + 0;
-				    var item = data[i].forWhat;
-				    var items_inr = inr;
-				    if(user_vise[name] != undefined)
-					inr = inr + Number(user_vise[name]); 
-				    if(item_vise[item] != undefined)
-					items_inr = items_inr + Number(item_vise[item]);
-				    user_vise[name] = inr;
-				    item_vise[item] = items_inr;
-				}
-                                res.render('status',{user_vise : user_vise, item_vise : item_vise, title : 'Status', params : req.query});
-                                });
-                        });
+    db.open(function(err, db) {
+        db.collection('expenses', function(err, collection) {
+            var search_query = {};
+            if (req.query.full != 'yes') {
+                var y = req.query.y ? req.query.y : new Date().getFullYear();
+                var m = req.query.m ? req.query.m : (new Date().getMonth() + 1);
+                var d = req.query.d ? req.query.d : '01';
+                req.query.y = y;
+                req.query.m = m;
+                var start = toDate(d + '/' + m + '/' + y);
+                if (m == 12) {
+                    y = parseInt(y) + 1;
+                    m = 0;
+                }
+                var ty = req.query.ty ? req.query.ty : y;
+                var tm = req.query.td ? (req.query.tm ? req.query.tm : m) : (Number(m) + 1);
+                var td = req.query.td ? req.query.td : '01';
+                var end = toDate(td + '/' + tm + '/' + ty);
+                search_query.date = {
+                    $gte: start,
+                    $lt: end
+                };
+            }
+            collection.find(search_query).toArray(function(err, data) {
+                db.close();
+                var user_vise = {};
+                var item_vise = {};
+                for (var i = 0; i < data.length; i++) {
+                    var name = data[i].name;
+                    var inr = Number(data[i].howMuch) + 0;
+                    var item = data[i].forWhat;
+                    var items_inr = inr;
+                    if (user_vise[name] != undefined)
+                        inr = inr + Number(user_vise[name]);
+                    if (item_vise[item] != undefined)
+                        items_inr = items_inr + Number(item_vise[item]);
+                    user_vise[name] = inr;
+                    item_vise[item] = items_inr;
+                }
+                res.render('status', {
+                    user_vise: user_vise,
+                    item_vise: item_vise,
+                    title: 'Status',
+                    params: req.query
                 });
-}
-
-var toDate = function(date_str){ 
-	var date = date_str.split('/');
-	return new Date(""+date[2] +'-'+ date[1] +'-'+ date[0]);
-}
-
-var insert_rec = function(req){
-db.open(function(err,db){
-        if(!err){
-          db.collection('expenses',function(err, collection){
-          if(!err){
-            //var date = req.body.date.split('/');
-            collection.insert({date : toDate(req.body.date),
-                               name : req.body.name,
-                               forWhat : req.body.forWhat,
-                               howMuch : req.body.howMuch },function(err){
-                                        db.close();
-                                        if(!err){
-                                                return true;
-                                        }
-                                });
-          }//end of collection if
-          });
-        }//end of open if
-        else{
-                console.log("Error: "+err)
-        }
+            });
         });
-	return false;
+    });
+}
+
+var toDate = function(date_str) {
+    var date = date_str.split('/');
+    return new Date("" + date[2] + '-' + date[1] + '-' + date[0]);
+}
+
+var insert_rec = function(req) {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection('expenses', function(err, collection) {
+                if (!err) {
+                    //var date = req.body.date.split('/');
+                    collection.insert({
+                        date: toDate(req.body.date),
+                        name: req.body.name,
+                        forWhat: req.body.forWhat,
+                        howMuch: req.body.howMuch
+                    }, function(err) {
+                        db.close();
+                        if (!err) {
+                            return true;
+                        }
+                    });
+                } //end of collection if
+            });
+        } //end of open if
+        else {
+            console.log("Error: " + err)
+        }
+    });
+    return false;
 }
 
 // under progress - not working
-var find_rec = function(id){
-	db.open(function(err,db){
-	if(!err){
-		db.collection('expenses', function(err,col){
-		if(!err){
-			col.find({ _id : id },function(rec){
-			db.close();
-			console.log(rec);
-			//if(!err){
-				return rec;
-			//}
-			});
-		}
-		});
-	}
-	});
-	return false;
+var find_rec = function(id) {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection('expenses', function(err, col) {
+                if (!err) {
+                    col.find({
+                        _id: id
+                    }, function(rec) {
+                        db.close();
+                        console.log(rec);
+                        //if(!err){
+                        return rec;
+                        //}
+                    });
+                }
+            });
+        }
+    });
+    return false;
 }
